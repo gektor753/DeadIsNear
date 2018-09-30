3a0cc967e4663f6acedd336b6a0ca0aaf232a472
Vlad <vlad.promotorov@studiomobile.ru>
2018-09-30 22:26:50 +0300

    Add buttons to start screen

--- app/src/main/java/com/var/deadisnear/MainActivity.kt
+++ app/src/main/java/com/var/deadisnear/MainActivity.kt
@@ -2,11 +2,31 @@ package com.`var`.deadisnear
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    val playButton: Button = findViewById(R.id.playButton)
+    val exitButton: Button = findViewById(R.id.exitButton)
+    val settingsButton: Button = findViewById(R.id.settingsButton)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playButton.setOnClickListener {
+            // TODO
+            print("Play")
+        }
+
+        exitButton.setOnClickListener {
+            // TODO
+            print("Exit")
+        }
+
+        settingsButton.setOnClickListener {
+            // TODO
+            print("Open settings")
+        }
     }
 }
